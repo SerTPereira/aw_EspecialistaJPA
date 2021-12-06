2e32388837f4539d8e2d1b387bca29b89bdede34
Sergio GT Pereira <sertpereira@gmail.com>
2021-12-06 10:47:49 -0300

    Atualizando objetos gerenciados

--- src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
+++ src/test/java/com/algaworks/ecommerce/iniciandocomjpa/OperacoesComTransacaoTest.java
@@ -13,6 +13,22 @@ import com.algaworks.ecommerce.model.Produto;
 public class OperacoesComTransacaoTest extends EntityManagerTest {
 
 	@Test
+	public void atualizarObjetoGerenciado() {
+		
+		Produto produto = entityManager.find(Produto.class, 1);
+				
+		entityManager.getTransaction().begin();
+		produto.setNome("Kindle Paperwhite 2º Geração");
+		entityManager.getTransaction().commit();
+		
+		entityManager.clear();
+		
+		Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+		assertEquals("Kindle Paperwhite 2º Geração", produtoVerificacao.getNome());
+		
+	}
+	
+	@Test
 	public void atualizarObjeto() {
 		
 		Produto produto = new Produto();
