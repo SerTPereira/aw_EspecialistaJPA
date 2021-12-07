9379ddde91c238c93adca685b9a92096d9b89afd
Sergio GT Pereira <sertpereira@gmail.com>
2021-12-07 14:05:36 -0300

    Exercício: mapeando relacionamentos um-para-muitos

--- src/main/java/com/algaworks/ecommerce/model/Pedido.java
+++ src/main/java/com/algaworks/ecommerce/model/Pedido.java
@@ -2,6 +2,7 @@ package com.algaworks.ecommerce.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Embedded;
@@ -13,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.EqualsAndHashCode;
@@ -52,5 +54,7 @@ public class Pedido {
 	@Embedded
 	private EnderecoEntregaPedido enderecoEntrega;
 
+	@OneToMany(mappedBy = "pedido")
+	private List<ItemPedido> itens;
 		
 }
