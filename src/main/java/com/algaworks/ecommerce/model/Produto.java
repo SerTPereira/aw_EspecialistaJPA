eb72a76595d2b6c0ace1fa185e0508983cae0cb9
Sergio GT Pereira <sertpereira@gmail.com>
2021-12-09 15:17:53 -0300

    Conhecendo detalhes da anotação @Column

--- src/main/java/com/algaworks/ecommerce/model/Produto.java
+++ src/main/java/com/algaworks/ecommerce/model/Produto.java
@@ -1,8 +1,10 @@
 package com.algaworks.ecommerce.model;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
@@ -15,7 +17,6 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.algaworks.ecommerce.listener.GenericListener;
-import com.algaworks.ecommerce.listener.GerarNotaFiscalListener;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -34,6 +35,12 @@ public class Produto {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
+	@Column(name = "data_criacao", updatable = false)
+	private LocalDateTime dataCriacao;
+	
+	@Column(name = "data_ultima_atualizacao", insertable = false)
+	private LocalDateTime dataUltimaAtualizacao;
+	
 	private String nome;
 
 	private String descricao;
